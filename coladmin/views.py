8915440216fd8be3d9899c2167fcfd576b79dea8
fernando velasquez <fvelasquez650@gmail.com>
2021-11-29 16:43:41 -0600

    added views and templates

--- coladmin/views.py
+++ coladmin/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .forms import MateriasForm
+from coladmin.models import Materias, Encurso
 
-# Create your views here.
+def materia_nueva(request):
+    if request.method == "POST":
+        formulario = MateriasForm(request.POST)
+        if formulario.is_valid():
+            materia = Materias.objects.create(nombremateria=formulario.cleaned_data['nombremateria'])
+            for alumno_id in request.POST.getlist('alumnos'):
+                encurso = Encurso(alumno_id=alumno_id, materia_id = materia.id)
+                encurso.save()
+            messages.add_message(request, messages.SUCCESS, 'Materia Guardada Exitosamente')
+    else:
+        formulario = MateriasForm()
+    return render(request, 'materia/materia_editar.html', {'formulario': formulario})
+
